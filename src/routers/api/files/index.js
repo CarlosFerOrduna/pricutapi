b4ead3b2300142fb3c710ad42341ade55ce23bf0
CarlosFerOrduna <fernandoorduna.co@gmail.com>
2024-02-26 19:25:10 -0300

    cambio permisos

--- src/routers/api/files/index.js
+++ src/routers/api/files/index.js
@@ -9,7 +9,7 @@ export class FileRouter extends BaseRouter {
         this.get('/download/:fid', ['public'], this.fileController.downloadFileById)
         this.get('/:fid/:pid', ['public'], this.fileController.getFileByIdWithPrice)
         this.get('/:fid', ['public'], this.fileController.getFileById)
-        this.post('/', ['admin'], this.fileController.saveFile)
+        this.post('/', ['public'], this.fileController.saveFile)
         this.put('/:fid', ['admin'], this.fileController.updateFile)
         this.delete('/:fid', ['admin'], this.fileController.deleteFile)
     }
